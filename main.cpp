67d6978aa0a04942a6537d9253f87c3fea958584
Heimen <3549107879@qq.com>
2023-09-26 15:41:14 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include"main.h"
 
+
+int show(int data[]){
+	for(int i=0;i<10;i++){
+		//TODO
+		printf("%d ",data[i]);
+	}
+	putchar('\n');
+}
 int main(int argc, char** argv) {
 //	SqlistTest();
 //	NodeTest();
@@ -8,6 +16,12 @@ int main(int argc, char** argv) {
 //	BSNodeTest();
 //	ThreadNodeTest();
 //	Queue.Test();
-	CSTNodeTest();
+//	CSTNodeTest();
+	
+	int data[10]={22,1,2,3,4,5,6,7,8,9};
+	int *p=data;
+	printf("%d\n",*(++p));
+	show(data);
+	printf("\n%d",*p);
 }
 
